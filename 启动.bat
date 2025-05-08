803d773a79ccc4da2b956a9511daf063342aa4b7
tabor <815141681@qq.com>
2025-05-08 15:30:17 +0800

    完善 启动.bat

--- 启动.bat
+++ 启动.bat
@@ -1,10 +1,7 @@
 @echo off
 pushd %~dp0
-REM %~dp0
-REM pushd
 echo Current directory is: %cd%
 echo The program is starting, please wait...
-rem cd /d F:\Code\TTS\index-tts
 set PYTHON_PATH=.\py312\python.exe
 echo The project is large and starts slowly, please wait patiently...
 timeout /t 3 /nobreak > nul 
