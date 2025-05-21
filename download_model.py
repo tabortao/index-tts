aa2df0b866f623a73140d8b200f22cb3b53de3ca
tabor <815141681@qq.com>
2025-05-21 09:13:14 +0800

    ### v0.0.3(20250521) - 更新：     - 优化模型下载文件download_model.py

--- download_model.py
+++ download_model.py
@@ -1,2 +1,5 @@
 from modelscope import snapshot_download
-snapshot_download('IndexTeam/IndexTTS-1.5', local_dir='checkpoints')
+# 下载1.5版本模型
+snapshot_download('IndexTeam/IndexTTS-1.5', local_dir='checkpoints/IndexTTS-1.5')
+# 下载1.0版本模型
+# snapshot_download('IndexTeam/Index-TTS', local_dir='checkpoints')
